7e70de19eefb2a94a9fb9ad0014d8220cf1ac001
I4AI3 <greta.maria.brugnatti.2007@calvino.edu.it>
2024-09-26 12:50:22 +0200

    questo è quello buono

--- main.c
+++ main.c
@@ -1,60 +1,91 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct{
-    char nome [60];
-    char cognome [100];
+    char nome[100];
+    char cognome [200];
     int altezza; //in centimetri
+} Alunno;
 
-}Alunno;
+void scriviSuFile(FILE * fp, Alunno a); //FUNZIONA
+void inizializzaAlunno(char nome[], char cognome[], int altezza, Alunno * pAlunno); //FUNZIONA
+void eliminaACapo(char stringa[]);
 
-void fileToString (char s[]);
+
 
 int main (){
-    FILE * fp;
-    int cont=0;
-    char ch='a';
-    int i=0;
-    char datiAlunno[100];
-    int scelta=-1;
+    //Dichiarazione delle variabili
+    FILE *fp;
     Alunno classe[40];
+    int cont=0;
+    int scelta;
+    char nome[100];
+    char cognome[200];
+    char file[1000];
+    int altezza;
+    char nomeFile[20];
 
-    do{
-        printf("\nGESTORE ALUNNI      \n");
-        printf("Seleziona un opzione: \n");
-        printf("1 - Aggiungi alunno   \n");
-        printf("2 - Elimina alunno    \n");
-        printf("3 - Modifica altezza  \n");
-        printf("4 - Visualizza alunno \n\n");
-        printf("0 - Esci              \n");
-        scanf("%d", &scelta);
 
-        switch(scelta){
-            case 1: //Aggiungi alunno
-                fp= fopen("classe/4aii.txt", "r");
-                if(fp == NULL){
-                    printf("Errore nell'apertura del file\n");
-                }
 
-                while(!feof(fp)){ //lettura della stringa di un file
-                    fgets(datiAlunno, 100, fp);
-                    while(ch!='|'&&i==3){ //Prende il nome
-                        ch=gets(datiAlunno);
-                        i++
+        printf("GESTORE ALUNNI\n");
+        printf("Digitare il nome completo del file: ");
+        fgets(nomeFile, sizeof(nomeFile), stdin);
+        eliminaACapo(nomeFile);
 
+        do{
+            printf("GESTORE ALUNNI\n");
+            printf("1 - Aggiungi utente \n");
+            printf("0 - Salva ed esci\n");
+            scanf("%d", &scelta);
+            fflush(stdin);
+            switch(scelta){
+                case 1: // Aggiungi utente
+                    //Dati alunno
+                    printf("Nome: ");
+                    fgets(nome, sizeof(nome), stdin);
+                    eliminaACapo(nome);
+                    fflush(stdin);
+
+                    printf("Cognome: ");
+                    fgets(cognome, sizeof(cognome), stdin);
+                    eliminaACapo(cognome);
+                    fflush(stdin);
+
+                    printf("Altezza in cm: ");
+                    scanf("%d", &altezza);
+                    fflush(stdin);
+
+                    inizializzaAlunno(nome, cognome, altezza, &classe[cont]);
+                    fp=fopen(nomeFile, "w"); //Apertura del file
+                    for(int i=0; i<cont; i++){
+                        scriviSuFile(fp, classe[i]);
                     }
-                    printf("%s", datiAlunno);
+                    fclose(fp); //Chiusura del file
                     cont++;
-                }
-                printf("\n");
+                    break;
+            }
+        }while (scelta!=0);
 
-                fclose(fp);
-                break;
+    if(fp==NULL){
+        printf("Errore nell'apertura del file \n");
+    }
 
-            case 2:
-                break;
-        }
+    printf("%d", cont);
 
-    }while(scelta!=0);
+}
+
+void scriviSuFile(FILE * fp, Alunno a){ //FUNZIONA
+    fprintf(fp, "%s ; %s ; %d cm\n", a.nome, a.cognome, a.altezza);
+}
+
+void inizializzaAlunno(char nome[], char cognome[], int altezza, Alunno * pAlunno){ //FUNZIONA
+    strcpy((*pAlunno).nome, nome);
+    strcpy((*pAlunno).cognome, cognome);
+    (*pAlunno).altezza = altezza;
+}
+
+void eliminaACapo(char stringa[]){
+    stringa[(strlen(stringa))-1]='\0';
 }
