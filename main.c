fd0fd702f6f5db23a81dcb2a0eb8efa02c576e50
I4AI3 <greta.maria.brugnatti.2007@calvino.edu.it>
2024-10-01 08:56:07 +0200

    ho rotto tutto

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ void scriviSuFile(FILE * fp, Alunno a); //FUNZIONA
 void inizializzaAlunno(char nome[], char cognome[], int altezza, Alunno * pAlunno); //FUNZIONA
 void eliminaACapo(char stringa[]); //FUNZIONA
 void caricaDaFile(Alunno * a, char line[]);
+void stampaAlunno(Alunno classe[], int cont);
+
 
 
 
@@ -20,8 +22,8 @@ int main (){
     //Dichiarazione delle variabili
     FILE *fp;
     Alunno classe[40];
+    int idAlunno=0;
     int cont=0;
-    int i=0;
     int scelta;
     char nome[100];
     char cognome[200];
@@ -40,11 +42,13 @@ int main (){
             printf("GESTORE ALUNNI\n");
             printf("1 - Aggiungi alunni \n");
             printf("2 - Visualizza alunni\n");
+            printf("3 - Modifica altezza\n");
             printf("0 - Salva ed esci\n");
             scanf("%d", &scelta);
             fflush(stdin);
             switch(scelta){
                 case 1: // Aggiungi utente
+
                     //Dati alunno
                     printf("Nome: ");
                     fgets(nome, sizeof(nome), stdin);
@@ -60,7 +64,9 @@ int main (){
                     scanf("%d", &altezza);
                     fflush(stdin);
 
+
                     inizializzaAlunno(nome, cognome, altezza, &classe[cont]);
+
                     fp=fopen(nomeFile, "ab"); //Apertura del file, con ab il contenuto verrà scritto dopo l'ultima riga
                                                             // senza dover sovrascrivere il file
                     scriviSuFile(fp, classe[cont]);
@@ -70,6 +76,7 @@ int main (){
                     break;
 
                 case 2:
+                    cont = 0;
                     fp = fopen(nomeFile, "r");
 
                     if (fp == NULL) {
@@ -78,12 +85,31 @@ int main (){
                     }
 
                     while(fgets(line, sizeof(line), fp) != NULL) {
-
                         caricaDaFile(&classe[cont], line);
+                        stampaAlunno(classe, cont);
                         cont++;
                     }
 
                         fclose(fp);
+                    break;
+
+                case 3: //Modifica altezza alunno
+                cont = 0;
+                    printf("Quale alunno vuoi modificare (0 - ...): \n");
+                fp=fopen(nomeFile, "r");
+                    while(fgets(line, sizeof(line), fp) != NULL) {
+                        caricaDaFile(&classe[cont], line);
+                        stampaAlunno(classe,cont);
+                        cont++;
+                    }
+
+                    fclose(fp);
+
+                    scanf("%d", &idAlunno);
+
+                    printf("Inserire nuova altezza: ");
+                    scanf("%d", &altezza);
+
                     break;
             }
         }while (scelta!=0);
@@ -118,16 +144,20 @@ void caricaDaFile(Alunno * a, char line[]){
     int altezza;
     token = strtok(line, ";");
     strcpy(nome, token);
-    printf("%s\n", token);
+    //printf("%s\n", token);
     token = strtok( NULL, ";" );
     strcpy(cognome, token);
-    printf("%s\n", token);
+    //printf("%s\n", token);
 
     token = strtok( NULL, ";" );
     strcpy(h, token);
 
     altezza=(int) atoll(h);
-    printf("%s\n", token);
+    //printf("%s\n", token);
 
     inizializzaAlunno(nome, cognome, altezza, a);
+}
+
+void stampaAlunno(Alunno classe[], int cont){
+    printf("%d  %s;%s;%d\n", cont, classe[cont].nome, classe[cont].cognome, classe[cont].altezza);
 }
