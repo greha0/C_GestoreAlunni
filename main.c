4eb3e29ce9eda1497e28fdc7ab6d3df323f88c85
I4AI3 <greta.maria.brugnatti.2007@calvino.edu.it>
2024-10-03 11:39:15 +0200

    funziona tutto, è solo da migliorare la grafica

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ typedef struct{
     int altezza; //in centimetri
 } Alunno;
 
+//AD OGNI SINGOLA OPERAZIONE, SALVARE
 void scriviSuFile(FILE * fp, Alunno a); //FUNZIONA
 void inizializzaAlunno(char nome[], char cognome[], int altezza, Alunno * pAlunno); //FUNZIONA
 void eliminaACapo(char stringa[]); //FUNZIONA
@@ -17,33 +18,42 @@ void stampaAlunno(Alunno classe[], int cont);
 
 
 
+//AD OGNI SINGOLA OPERAZIONE, SALVARE
+
 
 int main (){
     //Dichiarazione delle variabili
-    FILE *fp;
-    Alunno classe[40];
+
+    FILE *fp; //Puntatore al file
+    Alunno classe[40]; //Array di struct Alunno
     int idAlunno=0;
-    int cont=0;
+    int cont=0; //contatore di alunni
     int scelta;
     char nome[100];
     char cognome[200];
     int altezza;
-    char line[200];
+    char line[200]; //buffer per lettura file
     char nomeFile[20];
 
 
-
-        printf("GESTORE ALUNNI\n");
+// Menù
+        printf("+----------------+\n");
+        printf("| GESTORE ALUNNI |\n");
+        printf("+----------------+\n");
         printf("Digitare il nome completo del file: ");
         fgets(nomeFile, sizeof(nomeFile), stdin);
         eliminaACapo(nomeFile);
 
         do{
-            printf("GESTORE ALUNNI\n");
-            printf("1 - Aggiungi alunni \n");
-            printf("2 - Visualizza alunni\n");
-            printf("3 - Modifica altezza\n");
-            printf("0 - Salva ed esci\n");
+            printf("+-----------------------+\n");
+            printf("| GESTORE ALUNNI        |\n");
+            printf("+-----------------------+\n");
+            printf("| 1 - Aggiungi alunni   |\n");
+            printf("| 2 - Visualizza alunni |\n");
+            printf("| 3 - Modifica altezza  |\n");
+            printf("| 4 - Elimina alunno    |\n");
+            printf("| 0 - Salva ed esci     |\n");
+            printf("+-----------------------+\n\n");
             scanf("%d", &scelta);
             fflush(stdin);
             switch(scelta){
@@ -69,6 +79,12 @@ int main (){
 
                     fp=fopen(nomeFile, "ab"); //Apertura del file, con ab il contenuto verrà scritto dopo l'ultima riga
                                                             // senza dover sovrascrivere il file
+
+                    if (fp == NULL) {
+                        printf("Errore nell'apertura del file \n");
+                        break;
+                    }
+
                     scriviSuFile(fp, classe[cont]);
 
                     fclose(fp); //Chiusura del file
@@ -96,7 +112,13 @@ int main (){
                 case 3: //Modifica altezza alunno
                 cont = 0;
                     printf("Quale alunno vuoi modificare (0 - ...): \n");
-                fp=fopen(nomeFile, "r");
+                    fp=fopen(nomeFile, "r");
+
+                    if (fp == NULL) {
+                        printf("Errore nell'apertura del file \n");
+                        break;
+                    }
+
                     while(fgets(line, sizeof(line), fp) != NULL) {
                         caricaDaFile(&classe[cont], line);
                         stampaAlunno(classe,cont);
@@ -110,13 +132,61 @@ int main (){
                     printf("Inserire nuova altezza: ");
                     scanf("%d", &altezza);
 
+                    classe[idAlunno].altezza = altezza;
+
+                    fp = fopen(nomeFile, "w");
+
+                    if (fp == NULL) {
+                        printf("Errore nell'apertura del file \n");
+                        break;
+                    }
+
+                    for(int i=0; i<cont; i++){
+                        scriviSuFile(fp, classe[i]);
+                    }
+
+                    fclose(fp);
+
+                    break;
+
+                case 4: //Elimina alunno
+
+                    cont = 0;
+
+                    fp=fopen(nomeFile, "r");
+
+                    if (fp == NULL) {
+                        printf("Errore nell'apertura del file \n");
+                        break;
+                    }
+
+                    printf("Quale alunno vuoi eliminare?\n");
+                    while(fgets(line, sizeof(line), fp) != NULL) {
+                        caricaDaFile(&classe[cont], line);
+                        stampaAlunno(classe,cont);
+                        cont++;
+                    }
+                    fclose(fp);
+                    scanf("%d", &idAlunno);
+
+                    fp = fopen(nomeFile, "w");
+
+                    if (fp == NULL) {
+                        printf("Errore nell'apertura del file \n");
+                        break;
+                    }
+
+                    for(int i=0; i<cont; i++){
+                        if(i!=idAlunno) {
+                            scriviSuFile(fp, classe[i]);
+                        }
+                    }
+                    fclose(fp);
+
                     break;
             }
         }while (scelta!=0);
 
-    if(fp==NULL){
-        printf("Errore nell'apertura del file \n");
-    }
 
     printf("%d", cont);
 
